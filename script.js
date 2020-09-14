1c3cfb356469a718268386baad3aea26fc5930ab
Christian Marquez <christianmarquez41@gmail.com>
2020-09-13 21:42:03 -0400

    separated the starting div and the questions div into 2 different ones

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+// connecting the start button to this variable
+var startButton = document.getElementById("startBtn");
+// when start button is click, startGame function is called
+startButton.addEventListener("click", startGame);
+// connecting the start box to this variable
+var startBox = document.getElementById("startingBox");
+// connecting the question container to this variable
+var questionBox = document.getElementById("questionBox");
 
+
+
+// function that starts the game
+function startGame() {
+    startButton.classList.add("hide")
+
+
+}
 
 
 
