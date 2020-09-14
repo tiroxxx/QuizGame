10071edf089ffe47f70b84955bd4fada9e84a92d
Christian Marquez <christianmarquez41@gmail.com>
2020-09-13 22:00:51 -0400

    trying to get the start button to do something

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // connecting the start button to this variable
-var startButton = document.getElementById("startBtn");
+var startButton = document.getElementById("startBtn")
 // when start button is click, startGame function is called
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", startGame)
 // connecting the start box to this variable
-var startBox = document.getElementById("startingBox");
+var startBox = document.getElementById("startingBox")
 // connecting the question container to this variable
-var questionBox = document.getElementById("questionBox");
+var questionBox = document.getElementById("questionBox")
 
 
 
 // function that starts the game
 function startGame() {
-    startButton.classList.add("hide")
+    console.log("started")
+    startBox.classList.add("hide")
+    questionBox.classList.remove("hide")
 
 
 }
