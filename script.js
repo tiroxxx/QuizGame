0589d520dafa7a90b6ab09086612c7a7bbc8158d
Christian Marquez <christianmarquez41@gmail.com>
2020-09-13 20:46:54 -0400

    made an object that contains the questions, the choices, and the answers

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+
+
+var questions = [
+    {
+        title: "question1",
+        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
+        answer: "answer from choices"
+    },
+
+    {
+        title: "question2",
+        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
+        answer: "answer from choices"
+    },
+
+    {
+        title: "question3",
+        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
+        answer: "answer from choices"
+    },
+
+    {
+        title: "question4",
+        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
+        answer: "answer from choices"
+    },
+]
