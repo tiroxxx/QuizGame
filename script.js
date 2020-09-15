3936c1a7a19fa48ca25b3abcfd223ebf474cecbf
Christian Marquez <christianmarquez41@gmail.com>
2020-09-14 21:01:27 -0400

    wrote in all my questions for now

--- script.js
+++ script.js
@@ -12,31 +12,56 @@ var answerButton = document.querySelector(".answerBtn");
 var answerBox = document.getElementById("questionBox");
 
 var questionIndex = 0;
+var randomQuestions;
 
-
+// set of questions
 var questions = [
     {
-        title: "question1",
-        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
-        answer: "Choice 1"
+        title: "which actor is the only one who's appeared in every single Star Wars movie to date?",
+        choices: ["Carrie Fisher", "Mark Hamill", "Anthony Daniels", "Warwick Davis"],
+        answer: "Anthony Daniels"
     },
 
     {
-        title: "question2",
-        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
-        answer: "Choice 1"
+        title: "Which stormtrooper wasn't able to complete his mission in Star Wars: The Force Awakens?",
+        choices: ["FN-2705", "FN-2187", "FN-1205", "FN-1312"],
+        answer: "FN-2187"
     },
 
     {
-        title: "question3",
-        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
-        answer: "Choice 2"
+        title: "According to Master Yoda, how many Sith are always out there",
+        choices: ["Four", "One", "Three", "Two"],
+        answer: "Two"
     },
 
     {
-        title: "question4",
-        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
-        answer: "Choice 3"
+        title: "What happened to Anakin Skywalker during the battle with Count Dooku",
+        choices: ["He lost his left leg", "He lost his right arm", "He lost his right leg", "He won"],
+        answer: "He lost his right arm"
+    },
+
+    {
+        title: "What happened to Anakin Skywalker during the battle with Count Dooku",
+        choices: ["He lost his left leg", "He lost his right arm", "He lost his right leg", "He won"],
+        answer: "He lost his right arm"
+    },
+
+    {
+        title: "Who killed Jabba?",
+        choices: ["Han Solo", "C-3PO", "Luke Skywalker", "Princess Leia"],
+        answer: "Princess Leia"
+    },
+
+    {
+        title: "According to the Emperor, what was Luke Skywalker's weakness?",
+        choices: ["His faith in the Light Side of the Force", "His faith in his friends", "His lack of vision", "His resistance to the Dark Side of the Force"],
+        answer: "His faith in his friends"
+    },
+
+    {
+        title: "Where did the Clone Wars begin?",
+        choices: ["Tatooine", "Geonosis", "Naboo", "Coruscant"],
+        answer: "Geonosis"
     },
 ];
 
@@ -44,40 +69,18 @@ var questions = [
 
 // function that starts the game
 function startGame() {
-    console.log("started");
     // hide starting page
     startBox.classList.add("hide");
     // show question box
     questionBox.classList.remove("hide");
+    // randomize questions everytime the game starts
+    randomQuestions = 
     // set the questions
     startQuestions();
 
 }
 
+function createNextQuestion() {
+    
 
-
-var questions = [
-    {
-        title: "question1",
-        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
-        answer: "answer from choices"
-    },
-
-    {
-        title: "question2",
-        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
-        answer: "answer from choices"
-    },
-
-    {
-        title: "question3",
-        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
-        answer: "answer from choices"
-    },
-
-    {
-        title: "question4",
-        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
-        answer: "answer from choices"
-    },
-];
+}
