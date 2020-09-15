e8c790ab52c99a21931a4afb0bfd720a3b728277
Christian Marquez <christianmarquez41@gmail.com>
2020-09-14 20:31:42 -0400

    linked the answer buttons and question box to js variable

--- script.js
+++ script.js
@@ -6,15 +6,51 @@ startButton.addEventListener("click", startGame);
 var startBox = document.getElementById("startingBox");
 // connecting the question container to this variable
 var questionBox = document.getElementById("questionBox");
+// connecting answer buttons to this variable
+var answerButton = document.querySelector(".answerBtn");
+// connecting question box to this variable
+var answerBox = document.getElementById("questionBox");
+
+var questionIndex = 0;
+
+
+var questions = [
+    {
+        title: "question1",
+        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
+        answer: "Choice 1"
+    },
+
+    {
+        title: "question2",
+        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
+        answer: "Choice 1"
+    },
+
+    {
+        title: "question3",
+        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
+        answer: "Choice 2"
+    },
+
+    {
+        title: "question4",
+        choices: ["Choice 1", "Choice 2", "Choice3", "Choice 4"],
+        answer: "Choice 3"
+    },
+];
 
 
 
 // function that starts the game
 function startGame() {
     console.log("started");
+    // hide starting page
     startBox.classList.add("hide");
+    // show question box
     questionBox.classList.remove("hide");
-
+    // set the questions
+    startQuestions();
 
 }
 
