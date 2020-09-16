ce0cbf82f950fc0fe214cb92aca6d915e9c24425
Christian Marquez <christianmarquez41@gmail.com>
2020-09-16 01:01:47 -0400

    got the values to appear in the highscore leaderboard

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -9,6 +9,9 @@ goBack.addEventListener("click", function (event) {
     window.document.location = "index.html";
 })
 
+// initializing page
+init();
+
 function clearHighscores(event) {
     event.preventDefault();
     highscores = [];
@@ -18,7 +21,7 @@ function clearHighscores(event) {
 function init() {
     // get the stored initials
     var initialsObj = JSON.parse(localStorage.getItem("highscores"))
-    if (initialsObj !== NULL) {
+    if (initialsObj !== null) {
         //set object from storage to highscores obj
         highscores = initialsObj;
         //put highscores on the page
@@ -32,6 +35,12 @@ function renderHighscores() {
     // put in the values into the list
     for (var i = 0; i < highscores.score.length; i++) {
         var tempScore = highscores.score[i];
+        var tempInitial = highscores.playerInitials[i];
+        // create li to put in the values
+        var li = document.createElement("li");
+        li.textContent = tempInitial + " - " + tempScore;
+        // append li into ordered list
+        listEl.append(li);
     }
 }
 
